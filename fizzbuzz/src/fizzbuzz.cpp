4c4bf0bb955fe7b51cf1d2a47838336c6b704bd5
mhorsak <msiddharta1@gmail.com>
2024-10-01 11:50:54 +0200

    fizzbuzz: all tests added

--- fizzbuzz/src/fizzbuzz.cpp
+++ fizzbuzz/src/fizzbuzz.cpp
@@ -1,5 +1,6 @@
 #include <fizzbuzz.h>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -7,23 +8,27 @@ string fizzbuzz(int number){
 
     string fizzbuzz;
 
+    if( number <= 0){
+        return fizzbuzz = "Illegal number!";
+    }
+
     if ((number % 3 == 0)&& (number % 5 == 0)){
+        
+        return fizzbuzz = "fizzbuzz";
+    }
+
+    else if (number % 3 == 0){
+        return fizzbuzz= "fizz";
+
+    }
+
+    else if (number % 5 == 0){
+        return fizzbuzz = "buzz";
+    }
+
+    else{
         fizzbuzz = to_string(number);
-        return fizzbuzz;
     }
-
-
-    if (number % 3 == 0){
-        fizzbuzz = "fizz";
-        return fizzbuzz;
-
-    }
-
-    if (number % 5 == 0){
-        fizzbuzz = "buzz";
-        return fizzbuzz;
-    }
-
 
     return fizzbuzz;
 }
--- fizzbuzz/test/fizzbuzz_test.cpp
+++ fizzbuzz/test/fizzbuzz_test.cpp
@@ -14,7 +14,7 @@ TEST_CASE("Fizz the number is divisible by 3")
 }
 
 
-TEST_CASE("Fizz the number is divisible by 5")
+TEST_CASE("Buzz the number is divisible by 5")
 {
     string check1 = fizzbuzz(5);
     string check2 = fizzbuzz(10);
@@ -26,13 +26,35 @@ TEST_CASE("Fizz the number is divisible by 5")
 }
 
 
-TEST_CASE("Fizz the number is divisible by 3 AND 5")
+TEST_CASE("Fizzbuzz the number is divisible by 3 AND 5")
 {
     string check1 = fizzbuzz(15);
     string check2 = fizzbuzz(30);
     string check3 = fizzbuzz(60);
 
-    REQUIRE(check1 == "15");
-    REQUIRE(check2 == "30");
-    REQUIRE(check3 == "60");
+    REQUIRE(check1 == "fizzbuzz");
+    REQUIRE(check2 == "fizzbuzz");
+    REQUIRE(check3 == "fizzbuzz");
+}
+
+TEST_CASE("None of them above")
+{
+    string check1 = fizzbuzz(17);
+    string check2 = fizzbuzz(7);
+    string check3 = fizzbuzz(19);
+
+    REQUIRE(check1 == "17");
+    REQUIRE(check2 == "7");
+    REQUIRE(check3 == "19");
+}
+
+TEST_CASE("Illegal number 0 or <0")
+{
+    string check1 = fizzbuzz(0);
+    string check2 = fizzbuzz(-1);
+    string check3 = fizzbuzz(-3);
+
+    REQUIRE(check1 == "Illegal number!");
+    REQUIRE(check2 == "Illegal number!");
+    REQUIRE(check3 == "Illegal number!");
 }
