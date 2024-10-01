8b099311dc5feaa49e473ed83b18e7a0cc5e8212
mhorsak <msiddharta1@gmail.com>
2024-10-01 10:45:21 +0200

    Fizzbuzz error: test not working

--- fizzbuzz/src/fizzbuzz.cpp
+++ fizzbuzz/src/fizzbuzz.cpp
@@ -1,2 +1,15 @@
-#include 
+#include <fizzbuzz.h>
+
+string fizzbuzz(int number){
+
+    string fizzbuzz;
+
+    if (number % 3 == 0){
+        fizzbuzz = "fizz";
+    }
+
+    return fizzbuzz;
+}
+
+
 
---
+++ fizzbuzz/test/fizzbuzz_test.cpp
@@ -0,0 +1,14 @@
+#include <catch2/catch.hpp>
+#include <fizzbuzz.h>
+
+
+TEST_CASE("Fizz the number is divisible by 3")
+{
+    string check1 = fizzbuzz(3);
+   // string check2 = fizzbuzz(6);
+    //string check3 = fizzbuzz(9);
+
+    REQUIRE(check1 == "fizz");
+   // REQUIRE(check2 == "fizz");
+    //REQUIRE(check3 == "fizz");
+}
