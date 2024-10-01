bfe56fefc1679e4961dbfccf397ccff5e8f7d960
mhorsak <msiddharta1@gmail.com>
2024-10-01 11:02:27 +0200

    fizzbuzz basic done

--- fizzbuzz/src/fizzbuzz.cpp
+++ fizzbuzz/src/fizzbuzz.cpp
@@ -7,9 +7,23 @@ string fizzbuzz(int number){
 
     string fizzbuzz;
 
+    if ((number % 3 == 0)&& (number % 5 == 0)){
+        fizzbuzz = to_string(number);
+        return fizzbuzz;
+    }
+
+
     if (number % 3 == 0){
         fizzbuzz = "fizz";
+        return fizzbuzz;
+
     }
+
+    if (number % 5 == 0){
+        fizzbuzz = "buzz";
+        return fizzbuzz;
+    }
+
 
     return fizzbuzz;
 }
--- fizzbuzz/test/fizzbuzz_test.cpp
+++ fizzbuzz/test/fizzbuzz_test.cpp
@@ -5,10 +5,34 @@
 TEST_CASE("Fizz the number is divisible by 3")
 {
     string check1 = fizzbuzz(3);
-    // string check2 = fizzbuzz(6);
-    //string check3 = fizzbuzz(9);
+    string check2 = fizzbuzz(6);
+    string check3 = fizzbuzz(9);
 
     REQUIRE(check1 == "fizz");
-    // REQUIRE(check2 == "fizz");
-    //REQUIRE(check3 == "fizz");
+    REQUIRE(check2 == "fizz");
+    REQUIRE(check3 == "fizz");
+}
+
+
+TEST_CASE("Fizz the number is divisible by 5")
+{
+    string check1 = fizzbuzz(5);
+    string check2 = fizzbuzz(10);
+    string check3 = fizzbuzz(20);
+
+    REQUIRE(check1 == "buzz");
+    REQUIRE(check2 == "buzz");
+    REQUIRE(check3 == "buzz");
+}
+
+
+TEST_CASE("Fizz the number is divisible by 3 AND 5")
+{
+    string check1 = fizzbuzz(15);
+    string check2 = fizzbuzz(30);
+    string check3 = fizzbuzz(60);
+
+    REQUIRE(check1 == "15");
+    REQUIRE(check2 == "30");
+    REQUIRE(check3 == "60");
 }
