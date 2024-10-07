4404e7ab25b69e0f4157b51064cce12f4b773a2a
mhorsak <msiddharta1@gmail.com>
2024-10-07 13:51:02 +0200

    vector: random sort

--- vector/main.cpp
+++ vector/main.cpp
@@ -5,6 +5,27 @@
 
 
 const int NumberOfValues = 24;
+
+void sort(std::vector<int> &myVector)
+{
+    for(int pos = 0; pos < NumberOfValues; pos++)
+    {
+        int minValue = RAND_MAX; 
+        int swappos = 0;
+
+        for(int inner = pos; inner < NumberOfValues; inner++)
+        {
+            if (myVector[inner] < minValue)
+            {
+                swappos = inner;
+                minValue = myVector[inner];
+            }
+            myVector[swappos] = myVector[pos];
+            myVector[pos]= minValue;
+        }
+    }
+}
+
 int main()
 {
     srand(time(0));
@@ -25,6 +46,18 @@ int main()
 
      for(auto item: randomValues){
         std::cout << item << std::endl;
+    }
+
+    for(auto it = randomValues.begin(); it != randomValues.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    sort(randomValues);
+
+    for(auto it = randomValues.begin(); it != randomValues.end(); ++it)
+    {
+        std::cout << *it << std::endl;
     }
 
     return 0;
